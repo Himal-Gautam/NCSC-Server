99ba0e6b94a07eaf78011998dcde0b615eeb4a9a
Himal Gautam <himalpgautam@gmail.com>
2022-03-23 11:54:33 +0530

    port updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import './db/mongoose.js'
 import userRouter from './routers/user.js'
 import chalk from 'chalk'
 import cors from 'cors'
-// var cors = require('cors')
-// import taskRouter from './routers/task.js'
+import 'dotenv/config'
 
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
