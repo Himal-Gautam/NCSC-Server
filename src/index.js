57d6223ce5e2253bf923c926a725de37514ed272
Himal Gautam <himalpgautam@gmail.com>
2022-04-06 22:00:49 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
-import express from 'express'
-import './db/mongoose.js'
-import userRouter from './routers/user.js'
-import noticeRouter from './routers/notice.js'
-import subjectRouter from './routers/subject.js'
-import chalk from 'chalk'
-import cors from 'cors'
-import 'dotenv/config'
+import express from "express";
+import "./db/mongoose.js";
+import userRouter from "./routers/user.js";
+import noticeRouter from "./routers/notice.js";
+import subjectRouter from "./routers/subject.js";
+import chalk from "chalk";
+import cors from "cors";
+import "dotenv/config";
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
-app.use(userRouter)
-app.use(subjectRouter)
-app.use(noticeRouter)
+app.use(cors());
+app.use(express.json());
+app.use(userRouter);
+app.use(subjectRouter);
+app.use(noticeRouter);
 
 app.listen(port, () => {
-    console.log(chalk.magenta.bold.underline('Server is up on port ') + chalk.blue.bold.underline(port))
-})
+  console.log(
+    chalk.magenta.bold.underline("Server is up on port ") +
+      chalk.blue.bold.underline(port)
+  );
+});
+
+app.get("/", function (request, response) {
+  response.send("Welcome to NCSC Project Server");
+});
